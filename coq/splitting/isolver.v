d446b970488efe969fc091d935411b9071d7aa5b
B L <17168103+liesnikov@users.noreply.github.com>
2020-09-18 17:10:18 +0200

    add dummy isolver file

---
+++ coq/splitting/isolver.v
@@ -0,0 +1,7 @@
+From iris.bi Require Export bi telescopes.
+From iris.proofmode Require Export base classes notation.
+Set Default Proof Using "Type".
+
+From Local Require Import named_prop splitting_tactics splitting_ltac2_tactics imatch.
+
+Ltac2 Definition i_solver () := ().
